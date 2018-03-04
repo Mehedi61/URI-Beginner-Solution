39b6dc1c9c7bad24d869117a8f6d957cdf4d88f3
MD. Mehedi Hasan <emonmehedi61@gmail.com>
2018-03-05 00:46:18 +0600

    Accepted

---
+++ source/URI-1001.c
@@ -0,0 +1,11 @@
+// Programmed by MD. Mehedi Hasan
+#include <stdio.h>
+int main()
+{
+    int A,B,X;
+    scanf("%d",&A);
+    scanf("%d",&B);
+    X=A+B;
+    printf("X = %d\n",X);
+    return 0;
+}
