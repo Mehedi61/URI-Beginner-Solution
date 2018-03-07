88b272adf9bf537b8ead9feea15d4ba20fa71001
MD. Mehedi Hasan <emonmehedi61@gmail.com>
2018-03-08 00:09:45 +0600

    Accepted: Average 2

---
+++ source/URI-1006.c
@@ -0,0 +1,12 @@
+// Programmed by MD. Mehedi Hasan
+#include <stdio.h>
+int main()
+{
+    double a,b,c,MEDIA;
+    scanf("%lf",&a);
+    scanf("%lf",&b);
+    scanf("%lf",&c);
+    MEDIA=(a*2+b*3+c*5)/10;
+    printf("MEDIA = %0.1lf\n",MEDIA);
+    return 0;
+}
