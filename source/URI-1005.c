7a1b84e9329baf1b73d7cddc92551844edffb2e3
MD. Mehedi Hasan <emonmehedi61@gmail.com>
2018-03-08 00:02:49 +0600

    Accepted: Average 1

---
+++ source/URI-1005.c
@@ -0,0 +1,11 @@
+// Programmed by MD. Mehedi Hasan
+#include <stdio.h>
+int main()
+{
+    float a,b,MEDIA;
+    scanf("%f",&a);
+    scanf("%f",&b);
+    MEDIA=(a*3.5+b*7.5)/11;
+    printf("MEDIA = %0.5f\n",MEDIA);
+    return 0;
+}
