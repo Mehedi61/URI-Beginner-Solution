beb39678e0d70707583b0b7c4fb41fb84c54a2f5
MD. Mehedi Hasan <emonmehedi61@gmail.com>
2018-03-08 00:16:55 +0600

    Accepted: Salary

---
+++ source/URI-1008.c
@@ -0,0 +1,13 @@
+// Programmed by MD. Mehedi Hasan
+#include <stdio.h>
+int main()
+{
+    float NUMBER,hour,psalary,SALARY;
+    scanf("%f",&NUMBER);
+    scanf("%f",&hour);
+    scanf("%f",&psalary);
+    SALARY=hour*psalary;
+    printf("NUMBER = %0.0f\n",NUMBER);
+    printf("SALARY = U$ %0.2f\n",SALARY);
+    return 0;
+}
