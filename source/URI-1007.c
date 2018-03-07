eda5db519e46f973b557c26b7a2f7b8bd8d48437
MD. Mehedi Hasan <emonmehedi61@gmail.com>
2018-03-08 00:12:18 +0600

    Accepted: Difference

---
+++ source/URI-1007.c
@@ -0,0 +1,13 @@
+// Programmed by MD. Mehedi Hasan
+#include <stdio.h>
+int main()
+{
+    int A,B,C,D,DIFERENCA;
+    scanf("%d",&A);
+    scanf("%d",&B);
+    scanf("%d",&C);
+    scanf("%d",&D);
+    DIFERENCA=(A*B-C*D);
+    printf("DIFERENCA = %d\n",DIFERENCA);
+    return 0;
+}
