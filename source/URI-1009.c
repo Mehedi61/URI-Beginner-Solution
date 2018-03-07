50b2471b030d60a9043f320116475ab5e93a03f1
MD. Mehedi Hasan <emonmehedi61@gmail.com>
2018-03-08 00:37:39 +0600

    Accepted: Salary with Bonus

---
+++ source/URI-1009.c
@@ -0,0 +1,13 @@
+// Programmed by MD. Mehedi Hasan
+#include <stdio.h>
+int main()
+{
+    char ch[10];
+    double salary,sold,bonus=15,TOTAL;
+    scanf("%s",&ch);
+    scanf("%lf",&salary);
+    scanf("%lf",&sold);
+    TOTAL=salary+(bonus*sold)/100;
+    printf("TOTAL = R$ %0.2lf\n",TOTAL);
+    return 0;
+}
