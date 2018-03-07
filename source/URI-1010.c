0bd1f29a8c1a5caa90eb280a6348affccc5069a2
MD. Mehedi Hasan <emonmehedi61@gmail.com>
2018-03-08 00:54:40 +0600

    Accepted: Simple Calculate

---
+++ source/URI-1010.c
@@ -0,0 +1,11 @@
+// Programmed by MD. Mehedi Hasan
+#include <stdio.h>
+int main()
+{
+    float product1,unit1,price1,product2,unit2,price2,total;
+    scanf("%f %f %f",&product1,&unit1,&price1);
+    scanf("%f %f %f",&product2,&unit2,&price2);
+    total=(unit1*price1)+(unit2*price2);
+    printf("VALOR A PAGAR: R$ %0.2f\n",total);
+    return 0;
+}
