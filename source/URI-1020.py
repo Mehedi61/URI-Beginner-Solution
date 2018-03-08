4b51d3b4621b2d76502f34c2c81a06e657459702
MD. Mehedi Hasan <emonmehedi61@gmail.com>
2018-03-08 16:16:12 +0600

    Accepted: Age in Days

---
+++ source/URI-1020.py
@@ -0,0 +1,10 @@
+# Programmed by MD. Mehedi Hasan
+x = input()
+x = int(x)
+years = x / 365
+x = x % 365
+months = x / 30
+days = x % 30
+print("%d ano(s)" %years)
+print("%d mes(es)" %months)
+print("%d dia(s)" %days)
