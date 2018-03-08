6880719427e38cfc8a00d428c8ca6d2fe672eeb0
MD. Mehedi Hasan <emonmehedi61@gmail.com>
2018-03-08 15:58:05 +0600

    Accepted: Distance Between Two Points

---
+++ source/URI-1015.py
@@ -0,0 +1,11 @@
+// Programmed by MD. Mehedi Hasan
+x1, y1 = input().split()
+x2, y2 = input().split()
+x1, x2, y1, y2 = [float(x1), float(x2), float(y1), float(y2)]
+X = (x2 - x1) * (x2 - x1)
+Y = (y2 - y1) * (y2 - y1)
+add = X + Y
+float(add)
+import math
+add = math.sqrt(add)
+print("%.4f" %add)
