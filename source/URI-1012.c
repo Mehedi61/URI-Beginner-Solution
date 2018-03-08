17edefb8f3feee3fa9cc5d1e15184b978f29a6b1
MD. Mehedi Hasan <emonmehedi61@gmail.com>
2018-03-08 15:49:35 +0600

    Accepted: Area

---
+++ source/URI-1012.c
@@ -0,0 +1,8 @@
+// Programmed by MD. Mehedi Hasan
+A, B, C = input().split()
+A, B, C = [float(A), float(B), float(C)]
+print("TRIANGULO: %.3f" %(0.5*A*C))
+print("CIRCULO: %.3f" %(3.14159*C*C))
+print("TRAPEZIO: %.3f" %(0.5*(A+B)*C))
+print("QUADRADO: %.3f" %(B*B))
+print("RETANGULO: %.3f" %(A*B))
