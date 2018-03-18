dac5265bbb17c3738b4eeb4b03fcfea6076b2257
MD. Mehedi Hasan <emonmehedi61@gmail.com>
2018-03-19 00:58:14 +0600

    Accepted: Bhaskara's Formula

---
+++ source/URI-1036.c
@@ -0,0 +1,31 @@
+// Programmed by MD. Mehedi Hasan
+
+#include<stdio.h>
+#include<math.h>
+
+int main() {
+
+    double a, b, c, R1, R2, del;
+    scanf("%lf %lf %lf", &a, &b, &c);
+
+    del = (b * b) - (4 * a * c);
+
+    R1 = (- b + sqrt(del)) / (2 * a);
+    R2 = (- b - sqrt(del)) / (2 * a);
+
+    if(a != 0 && del > 0) {
+
+        printf("R1 = %.5lf\n", R1);
+	printf("R2 = %.5lf\n", R2);
+	    
+    }
+
+    else {
+
+        printf("Impossivel calcular\n");
+	    
+    }
+
+    return 0;
+    
+}
