10c187a709743cc0c2618972b341155361a099e5
MD. Mehedi Hasan <emonmehedi61@gmail.com>
2018-03-29 23:50:19 +0600

    Accepted: TDA Rational

---
+++ source/URI-1022.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <math.h>
+
+int mdc(int a, int b)
+{
+	if(a<0)
+		a = -a;
+	if(b<0)
+		b = -b;
+	if(a%b == 0)
+		return b;
+	else
+		return mdc(b,a%b);
+}
+
+
+int main (){
+
+	int N, n1, d1, n2, d2, num, den, div;
+	char oper;
+
+	scanf("%d", &N);
+
+	do{
+		scanf("%d / %d %c %d / %d", &n1, &d1, &oper, &n2, &d2);
+
+		if(oper == '+'){
+			num = (n1*d2 + n2*d1);
+			den = (d1*d2);
+		}
+
+		else if(oper == '-'){
+			num = (n1*d2 - n2*d1);
+			den = (d1*d2);
+		}
+
+		else if(oper == '*'){
+			num = (n1*n2);
+			den = (d1*d2);
+		}
+
+		else if(oper == '/'){
+			num = (n1*d2);
+			den = (n2*d1);
+		}
+
+		div = mdc(num,den);
+
+		printf("%d/%d = %d/%d\n", num, den, num/div, den/div);
+
+		N--;
+	}
+	
+	while (N>0);
+	
+		return 0;
+}
