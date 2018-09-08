85b94596d6e2a033e62bf1e5b62fac50a7e38a6d
MD. Mehedi Hasan <emonmehedi61@gmail.com>
2018-09-08 06:53:03 +0600

    Accepted: Easy Fibonacci

---
+++ source/URI-1151.py
@@ -0,0 +1,19 @@
+number = int(input())
+
+if number >= 46:
+    exit()
+
+x = 0
+y = 1
+z = 3
+
+print("{} {}".format(x,y),end = "")
+
+while number>=z and number<46:
+    xy = x + y
+    print(" {}".format(xy),end= "")
+    x = y
+    y = xy
+    z += 1
+
+print()
